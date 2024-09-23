82677beea7fa75d941ae30ef550accb437529632
Maxleon Gustafsson <Maxleon_lappalainen@hotmail.com>
2024-09-23 18:46:52 +0200

    skapade ett dåligt program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Skriv in 1,2 eller valfri siffra");
+        int input = int.Parse(Console.ReadLine());
+        bool running = true;
+        while (running)
+        if (input == 1)
+        {
+            System.Console.WriteLine("Du tryckte på 1");
+            break;
+
+        }
+        else if (input == 2)
+        {
+            System.Console.WriteLine("Du tryckte på 2");
+            break;
+        }
+        else 
+        {
+            running = false;
+        }
+
+    }
+}
